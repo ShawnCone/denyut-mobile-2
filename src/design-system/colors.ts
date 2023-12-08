a34fe84c87050c2ba9350b99e2feaa83527a7761
ShawnCone <deandre.dave@gmail.com>
2023-12-07 19:03:02 -0800

    set up design tokens

---
+++ src/design-system/colors.ts
@@ -0,0 +1,46 @@
+// Each color has 5 variations (Except for neutral colors): extra light, light, normal, dark, extra dark
+// Most of these colors are taken from https://tailwindcss.com/docs/customizing-colors
+export const colors = {
+  primary: {
+    extraLight: '#d1fae5',
+    light: '#6ee7b7',
+    normal: '#10b981',
+    dark: '#047857',
+    extraDark: '#064e3b',
+  },
+  destructive: {
+    extraLight: '#fee2e2',
+    light: '#fca5a5',
+    normal: '#ef4444',
+    dark: '#b91c1c',
+    extraDark: '#7f1d1d',
+  },
+  warning: {
+    extraLight: '#fef3c7',
+    light: '#fcd34d',
+    normal: '#f59e0b',
+    dark: '#b45309',
+    extraDark: '#78350f',
+  },
+  success: {
+    extraLight: '#dcfce7',
+    light: '#86efac',
+    normal: '#22c55e',
+    dark: '#15803d',
+    extraDark: '#14532d',
+  },
+  info: {
+    extraLight: '#e0f2fe',
+    light: '#7dd3fc',
+    normal: '#0ea5e9',
+    dark: '#0369a1',
+    extraDark: '#0c4a6e',
+  },
+  neutral: {
+    extraLight: '#f1f5f9',
+    light: '#cbd5e1',
+    normal: '#64748b',
+    dark: '#334155',
+    extraDark: '#0f172a',
+  },
+}
---
+++ src/design-system/font-sizing.ts
@@ -0,0 +1,38 @@
+export const fontSizing = {
+  XS: {
+    fontSize: 12,
+    lineHeight: 16,
+  },
+  S: {
+    fontSize: 14,
+    lineHeight: 20,
+  },
+  M: {
+    fontSize: 16,
+    lineHeight: 24,
+  },
+  L: {
+    fontSize: 18,
+    lineHeight: 28,
+  },
+  XL: {
+    fontSize: 20,
+    lineHeight: 28,
+  },
+  XXL: {
+    fontSize: 24,
+    lineHeight: 32,
+  },
+  '3XL': {
+    fontSize: 30,
+    lineHeight: 36,
+  },
+  '4XL': {
+    fontSize: 36,
+    lineHeight: 40,
+  },
+  '5XL': {
+    fontSize: 48,
+    lineHeight: 48,
+  },
+}
