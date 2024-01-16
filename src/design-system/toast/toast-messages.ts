e1cd2c69b580beba5ed2d702b5ef4f7ab0e966a4
ShawnCone <deandre.dave@gmail.com>
2024-01-15 23:19:53 -0800

    add calls for skdn generation screen

--- src/design-system/toast/toast-messages.ts
+++ src/design-system/toast/toast-messages.ts
@@ -9,3 +9,8 @@ export const SUCCESSFULLY_JOINED_POSYANDU = 'Berhasil bergabung dengan posyandu'
 
 // Print related messages
 export const ERROR_PRINTING_GROWTH_RECORD = 'Gagal mencetak data pertumbuhan'
+
+// Report related messages
+export const UNABLE_TO_DOWNLOAD_REPORT = 'Tidak dapat mengunduh laporan'
+export const UNABLE_TO_SAVE_REPORT = 'Tidak dapat menyimpan laporan'
+export const SUCCESSFULLY_SAVED_REPORT = 'Berhasil menyimpan laporan'
